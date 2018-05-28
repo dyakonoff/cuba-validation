afe9df87cdcca076dadf52443ea23b6a830b732f
Mikhail Dyakonov <dyakonov.mikhail@gmail.com>
2018-05-28 22:57:51 +0400

    Fixing transaction listener

--- orderman/modules/core/src/com/haulmont/dyakonoff/orderman/service/TransactionListener.java
+++ orderman/modules/core/src/com/haulmont/dyakonoff/orderman/service/TransactionListener.java
@@ -35,85 +35,90 @@ public class TransactionListener implements BeforeCommitTransactionListener {
      */
     @Override
     public void beforeCommit(EntityManager entityManager, Collection<Entity> managedEntities) {
-        return;
-//
-//        // see https://doc.cuba-platform.com/manual-6.8/transaction_listeners.html for more examples
-//        PersistenceTools persistenceTools = persistence.getTools();
-//
-//        // building a list of orders to commit
-//        ArrayList<Order> ordersToCheck = new ArrayList<>();
-//        for (Entity entity : managedEntities) {
-//
-//            if (persistenceTools.isDirty(entity))
-//                continue;
-//            if (entity instanceof Order) {
-//                ordersToCheck.add( (Order)entity );
-//            }
-//            else if (entity instanceof OrderItem) {
-//                Order order = ((OrderItem)entity).getOrder();
-//                // a reference can be detached, so merge it into current persistence context
-//                ordersToCheck.add(entityManager.merge(order));
-//                ordersToCheck.add(order);
-//            }
-//        }
-//
-//        HashMap<UUID, BigDecimal> stockChanges = new HashMap<>();
-//        // building a set of products to be removed & returned from the stock
-//        for (Order order : ordersToCheck) {
-//            BigDecimal orderMultiplicator = new BigDecimal( getOrderMultiplier(order, persistenceTools));
-//            for (OrderItem item : order.getItems()) {
-//                UUID productId = item.getProduct().getId();
-//                BigDecimal newQty = item.getQuantity();
-//                BigDecimal oldQty = (BigDecimal)persistenceTools.getOldValue(item, "quantity");
-//
-//                if (newQty == null) newQty = BigDecimal.ZERO;
-//                if (oldQty == null) oldQty = BigDecimal.ZERO;
-//                BigDecimal qtyChange = newQty.subtract(oldQty).multiply(orderMultiplicator);
-//                if (qtyChange.compareTo(BigDecimal.ZERO) != 0) {
-//                    // we have changes in Stock for this OrderItem
-//                    if (stockChanges.containsKey(productId))
-//                        stockChanges.merge(productId, qtyChange, BigDecimal::add);
-//                    else
-//                        stockChanges.put(productId, qtyChange);
-//                }
-//            }
-//        }
-//
-//        // checking that are enough values in stock
-//        // changes stock
-//        try (Transaction tx = persistence.createTransaction()) {
-//            TypedQuery<Stock> query = persistence.getEntityManager().createQuery(
-//                    "SELECT s FROM orderman$Stock s WHERE s.product IN :products_set", Stock.class);
-//
-//            Set<UUID> productIds = stockChanges.keySet();
-//            ArrayList<String> productStringIds = new ArrayList<>(productIds.size());
-//            productIds.forEach(id -> productStringIds.add(id.toString()));
-//            query.setParameter("products_set", productStringIds);
-//
-//            List<Stock> stocks = query.getResultList();
-//
-//            for (Stock s : stocks) {
-//                BigDecimal valChange = stockChanges.get(s.getProduct().getId());
-//                BigDecimal newStockVal = s.getInStock().add( valChange );
-//                if (newStockVal.compareTo(BigDecimal.ZERO) < 0) {
-//                    String msg = "Insufficient amount of product '" + s.getProduct().getName() + "' in Stock, "
-//                            + s.getInStock().toString() + " left, required: " + valChange.toString();
-//                    throw new ValidationException(msg);
-//                }
-//                s.setInStock(newStockVal);
-//            }
-//            tx.commit();
-//        }
-//
-//        //
+
+        // see https://doc.cuba-platform.com/manual-6.8/transaction_listeners.html for more examples
+        PersistenceTools persistenceTools = persistence.getTools();
+
+        // building a list of orders to commit
+        Set<Order> ordersToCheck = new HashSet<>();
+        for (Entity entity : managedEntities) {
+
+            if (!persistenceTools.isDirty(entity))
+                continue;
+            if (entity instanceof Order) {
+                ordersToCheck.add( (Order)entity );
+            }
+            else if (entity instanceof OrderItem) {
+                Order order = ((OrderItem)entity).getOrder();
+                // a reference can be detached, so merge it into current persistence context
+                ordersToCheck.add(entityManager.merge(order));
+                ordersToCheck.add(order);
+            }
+        }
+
+        if (ordersToCheck.size() == 0)
+            return; // nothing to do
+
+        HashMap<UUID, BigDecimal> stockChanges = new HashMap<>();
+        // building a set of products to be removed & returned from the stock
+        for (Order order : ordersToCheck) {
+            BigDecimal orderMultiplicator = new BigDecimal( getOrderMultiplier(order, persistenceTools));
+            for (OrderItem item : order.getItems()) {
+                UUID productId = item.getProduct().getId();
+                BigDecimal newQty = item.getQuantity();
+                BigDecimal oldQty = (BigDecimal)persistenceTools.getOldValue(item, "quantity");
+
+                if (newQty == null) newQty = BigDecimal.ZERO;
+                if (oldQty == null) oldQty = BigDecimal.ZERO;
+                BigDecimal qtyChange = newQty.subtract(oldQty).multiply(orderMultiplicator);
+                if (qtyChange.compareTo(BigDecimal.ZERO) != 0) {
+                    // we have changes in Stock for this OrderItem
+                    if (stockChanges.containsKey(productId))
+                        stockChanges.merge(productId, qtyChange, BigDecimal::add);
+                    else
+                        stockChanges.put(productId, qtyChange);
+                }
+            }
+        }
+
+        if (stockChanges.size() == 0)
+            return; // nothing to do
+
+        // checking that are enough values in stock
+        // changes stock
+        try (Transaction tx = persistence.createTransaction()) {
+            TypedQuery<Stock> query = persistence.getEntityManager().createQuery(
+                    "SELECT s FROM orderman$Stock s WHERE s.product IN :products_set", Stock.class);
+
+            Set<UUID> productIds = stockChanges.keySet();
+            ArrayList<String> productStringIds = new ArrayList<>(productIds.size());
+            productIds.forEach(id -> productStringIds.add(id.toString()));
+            query.setParameter("products_set", productStringIds);
+
+            List<Stock> stocks = query.getResultList();
+
+            for (Stock s : stocks) {
+                BigDecimal valChange = stockChanges.get(s.getProduct().getId());
+                BigDecimal newStockVal = s.getInStock().add( valChange );
+                if (newStockVal.compareTo(BigDecimal.ZERO) < 0) {
+                    String msg = "Insufficient amount of product '" + s.getProduct().getName() + "' in Stock, "
+                            + s.getInStock().toString() + " left, required: " + valChange.multiply(new BigDecimal(-1)).toString();
+                    throw new ValidationException(msg);
+                }
+                s.setInStock(newStockVal);
+            }
+            tx.commit();
+        }
+
+        //
     }
 
 
     private static int[][] orderTransMatrix = {
-            {  0, +1, +1,  0 },
-            { -1,  0,  0, -1 },
-            { -1,  0,  0, -1 },
-            {  0, +1, +1,  0 }
+            {  0, -1, -1,  0 },
+            { +1,  0,  0, +1 },
+            { +1,  0,  0, +1 },
+            {  0, -1, -1,  0 }
     };
 
 
@@ -126,14 +131,32 @@ public class TransactionListener implements BeforeCommitTransactionListener {
     private int getOrderMultiplier(Order order, PersistenceTools pt) {
         /*
                      | null | NEW | PAID | CANCELED |
-            null     |   0    +1    +1        0
-            NEW      |  -1     0     0       -1
-            PAID     |  -1     0     0       -1
-            CANCELED |   0    +1    +1       0
+            null     |   0    -1    -1        0
+            NEW      |  +1     0     0       +1
+            PAID     |  +1     0     0       +1
+            CANCELED |   0    -1    -1        0
+
+            How to read this
+            rows => initial order state (before transaction)
+            columns => final order state (after transaction)
+
+            For example:
+            * if order was created (comes from null to NEW), then all his OrderItem.quantity should be substracted from Stock.inStock == Multiplier == -1
+            * if order was paid (comes from NEW to PAID), nothing changed, that's whi element [1, 2] == 0 (row, col, 0-based)
+            * If order was deleted from PAID status (comes from PAID to null) than all order items should be returned to stock ==> multiplier === +1
         */
 
-        int state_1 = orderStatusToInt(order, (OrderStatus) pt.getOldValue(order, "status"));
-        int state_2 = orderStatusToInt(order, order.getStatus());
+        int state_1, state_2;
+        if (!order.isDeleted()) {
+            // regular case
+            state_1 = orderStatusToInt(order, OrderStatus.fromId((Integer) pt.getOldValue(order, "status")));
+            state_2 = orderStatusToInt(order, order.getStatus());
+        }
+        else {
+            // if order goes to be deleted, then his old state is always == null and actually, it goes from it's current state to null
+            state_1 = orderStatusToInt(order, order.getStatus());
+            state_2 = 0;
+        }
         return orderTransMatrix[state_1][state_2];
     }
 
